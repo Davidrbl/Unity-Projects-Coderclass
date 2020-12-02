3eef47d1bc3adecf51f084cba92d9843c0c3be6f
Davidrbl <38097012+Davidrbl@users.noreply.github.com>
2020-12-02 10:33:17 +0100

    Begonnen met impulse

--- Specialisatie-1/Specialisatie-1 David Rebel/Assets/Scripts/ImpulseScript.cs
+++ Specialisatie-1/Specialisatie-1 David Rebel/Assets/Scripts/ImpulseScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ImpulseScript : MonoBehaviour
 {
+    private float ImpulseStrenth = 50f;
+    private float ImpulseRadius = 5f;
+    private Transform tf;
     // Start is called before the first frame update
     void Start()
     {
-        
+      tf = GetComponent<Transform>();
+      int maxColliders = 10;
+      Collider[] hitColliders = new Collider[maxColliders];
+      int numColliders = Physics.OverlapSphereNonAlloc(tf.position, ImpulseRadius, hitColliders);
+      for (int i = 0; i < numColliders; i++)
+      {
+          //force adden aan alle gameObjects met rigidbodies
+          //hitColliders[i].GetComponent<Rigidbody>().AddForce;
+      }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
